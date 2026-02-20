ec51303fd7ef7e946d9d8843c63464a508446947
nKOxxx <98685501+nKOxxx@users.noreply.github.com>
2026-02-20 18:58:05 +0400

    Fix CSP: Allow Google Fonts (styles and fonts)

--- src/index.js
+++ src/index.js
@@ -42,7 +42,8 @@ app.use(helmet({
     directives: {
       defaultSrc: ["'self'"],
       scriptSrc: ["'self'", "'unsafe-inline'"],
-      styleSrc: ["'self'", "'unsafe-inline'"],
+      styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
+      fontSrc: ["'self'", "https://fonts.gstatic.com"],
       imgSrc: ["'self'", "data:", "https:"],
       connectSrc: ["'self'", "ws:", "wss:"],
     },
